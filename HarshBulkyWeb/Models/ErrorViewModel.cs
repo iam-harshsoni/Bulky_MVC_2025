6682e3b477551d5440636384e1f8dd2b544c0538
harsh soni <harshsoni6011@gmail.com>
2025-04-03 10:32:08 +0530

    Add project files.

---
+++ HarshBulkyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HarshBulkyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
