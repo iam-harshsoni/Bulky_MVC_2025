f020c645e3a47b9ae86c7638291ce102d39be425
harsh soni <harshsoni6011@gmail.com>
2025-04-03 19:42:26 +0530

    feat: Added toastr message notification for all the CRUD Operations

--- HarshBulkyWeb/Controllers/CategoryController.cs
+++ HarshBulkyWeb/Controllers/CategoryController.cs
@@ -37,6 +37,7 @@ namespace HarshBulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully!";
                 return RedirectToAction("Index");
             }
             return View();
@@ -63,6 +64,7 @@ namespace HarshBulkyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully!";
                 return RedirectToAction("Index");
 
             }
@@ -92,6 +94,7 @@ namespace HarshBulkyWeb.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully!";
             return RedirectToAction("Index");
 
         }
