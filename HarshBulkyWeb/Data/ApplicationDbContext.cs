dfeb93e494655592b5b2fd115a2045bd2107ba37
harsh soni <harshsoni6011@gmail.com>
2025-04-03 15:06:52 +0530

    feat: configured ApplicationDBContext with SQL Server and created database using nudget package manager console

---
+++ HarshBulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HarshBulkyWeb.Data
+{
+   //Primary Constructor
+   public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+
+    }
+}
--- HarshBulkyWeb/Program.cs
+++ HarshBulkyWeb/Program.cs
@@ -1,7 +1,18 @@
+using HarshBulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure ApplicationDbContext to use SQL Server database
+// Register ApplicationDbContext with SQL Server as the database provider
+// Ensure the connection string is properly set in the appsettings.json file
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+
+
 
 var app = builder.Build();
 
