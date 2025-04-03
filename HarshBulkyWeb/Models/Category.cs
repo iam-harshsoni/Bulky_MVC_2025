94d9f02434f3fcae49f27ac533de094809ca2a8e
harsh soni <harshsoni6011@gmail.com>
2025-04-03 12:15:53 +0530

    feat: added Category Model

---
+++ HarshBulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HarshBulkyWeb.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }  // if the name is Id or CategoryId, then .NET CORE will treat it as a PK, so no need to write [Key] above, if the name is something else, then we have to use [Key] to explicitly specify that its a PK.
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+    }
+}
