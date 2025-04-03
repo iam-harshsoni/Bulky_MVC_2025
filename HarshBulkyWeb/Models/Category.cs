0d59962959f8e31a072af5dcd2201440058e5b55
harsh soni <harshsoni6011@gmail.com>
2025-04-03 17:15:42 +0530

    style: Changed the style. Used tag helper to link UI elements from model

--- HarshBulkyWeb/Models/Category.cs
+++ HarshBulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace HarshBulkyWeb.Models
 {
@@ -6,7 +7,10 @@ namespace HarshBulkyWeb.Models
     {
         public int CategoryId { get; set; }  // if the name is Id or CategoryId, then .NET CORE will treat it as a PK, so no need to write [Key] above, if the name is something else, then we have to use [Key] to explicitly specify that its a PK.
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+
+        [DisplayName("Display Name")]
         public int DisplayOrder { get; set; }
     }
 }
