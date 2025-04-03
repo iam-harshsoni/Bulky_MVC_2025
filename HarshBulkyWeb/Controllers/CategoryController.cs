3c1431201b0d80a8e640e4d20a5db7dfcaa269bc
harsh soni <harshsoni6011@gmail.com>
2025-04-03 19:04:21 +0530

    feat: Completed edit functionality for Categories

--- HarshBulkyWeb/Controllers/CategoryController.cs
+++ HarshBulkyWeb/Controllers/CategoryController.cs
@@ -21,7 +21,7 @@ namespace HarshBulkyWeb.Controllers
         }
 
         public IActionResult Create()
-        {            
+        {
             return View();
         }
 
@@ -38,9 +38,56 @@ namespace HarshBulkyWeb.Controllers
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
-
             }
             return View();
         }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+
+            var categoryFromDb = _db.Categories.Find(id);
+            //  var categoryFromDb1 = _db.Categories.FirstOrDefault(x=>x.CategoryId == id); //best approach
+            //  var categoryFromDb3 = _db.Categories.Where(x => x.CategoryId == id).FirstOrDefault();  // only when there is any customization
+
+            if (categoryFromDb == null) { return NotFound(); }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            /*  
+                Note: 
+                    - Have to add <input hidden asp-for="CategoryId" /> to populate id here. 
+                    - If the field name is Id then there is no need to write this in the view but 
+                      in my model, I have mentioned 'CategoryId' so I have to add this <input > to get the ID here. 
+             
+             */
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+
+            var categoryFromDb = _db.Categories.Find(obj.CategoryId);
+
+            if (categoryFromDb == null) { return NotFound(); }
+
+            categoryFromDb.Name = obj.Name;
+            categoryFromDb.DisplayOrder = obj.DisplayOrder;
+
+            _db.SaveChanges();
+
+            return View();
+        }
+
+
     }
 }
