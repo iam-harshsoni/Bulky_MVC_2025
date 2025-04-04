a34ebd3bc4c48c9a3de5e081f8531ae6296704f1
harsh soni <harshsoni6011@gmail.com>
2025-04-04 20:35:52 +0530

    feat: Created a new Razor Page application and configured database setting and connectionString. Created Category Model class and ApplicationDbContext.class

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Category> Categories {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category { CategoryId = 1, Name = "SciFi", DisplayOrder = 1 },
+                 new Category { CategoryId = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { CategoryId = 3, Name = "History", DisplayOrder = 3 }
+            );
+        }
+    }
+}
---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }  // if the name is Id or CategoryId, then .NET CORE will treat it as a PK, so no need to write [Key] above, if the name is something else, then we have to use [Key] to explicitly specify that its a PK.
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
