5fd16be56cae9b085d4aa734568656c3708a0634
harsh soni <harshsoni6011@gmail.com>
2025-04-05 21:43:10 +0530

    feat: Product list and create and edit functionality added

---
+++ HarshBulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using HarshBulky.DataAccess.Repository.IRepository;
+using HarshBulky.Models;
+using Humanizer.Localisation.DateToOrdinalWords;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+
+namespace HarshBulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully!";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully!";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+    }
+}
