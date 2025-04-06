1667eaf51fdc4b98ba0af80ffca56e9d09c522a7
harsh soni <harshsoni6011@gmail.com>
2025-04-06 16:34:20 +0530

    feat: Image upload implemented and saved in folder as well as image url save in db

--- HarshBulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ HarshBulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -13,11 +13,12 @@ namespace HarshBulkyWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
-
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -58,6 +59,24 @@ namespace HarshBulkyWeb.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+                if (file != null)
+                {
+                    // saving uploaded file into folder.
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.Name);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    // saving the Image URL to Database
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
+
                 _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully!";
@@ -77,7 +96,7 @@ namespace HarshBulkyWeb.Areas.Admin.Controllers
             }
             return View();
         }
-         
+
         public IActionResult Delete(int id)
         {
             Product product = _unitOfWork.Product.Get(u => u.Id == id);
