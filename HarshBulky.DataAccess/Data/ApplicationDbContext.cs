faf177c27fcfac3420cfa93c6ee53357b4b72d16
harsh soni <harshsoni6011@gmail.com>
2025-04-07 12:24:04 +0530

    feat: extended columns on default IdentityUser

--- HarshBulky.DataAccess/Data/ApplicationDbContext.cs
+++ HarshBulky.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace HarshBulky.DataAccess.Data
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -96,6 +97,7 @@ namespace HarshBulky.DataAccess.Data
                     ImageUrl = ""
                 }
             );
+
         }
     }
 }
