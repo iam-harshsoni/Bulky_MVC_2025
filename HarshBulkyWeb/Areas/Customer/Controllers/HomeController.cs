054eb9b411a6bdc1022f43c617682a105df2cf90
harsh soni <harshsoni6011@gmail.com>
2025-04-07 11:06:22 +0530

    feat: add list of product and details of product for client side

--- HarshBulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ HarshBulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using HarshBulky.DataAccess.Repository.IRepository;
 using HarshBulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,23 @@ namespace HarshBulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category"); 
+            return View(productList);
+        }
+
+        public IActionResult Details(int? productId)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties:"Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
