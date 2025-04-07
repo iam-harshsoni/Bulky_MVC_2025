0a0c15a82f846660115dd84f5fb01f26fd3e3f95
harsh soni <harshsoni6011@gmail.com>
2025-04-07 12:11:21 +0530

    feat: added migration and identity tables in the database.

--- HarshBulky.DataAccess/Migrations/20250407063743_addIdentityTables.Designer.cs
+++ HarshBulky.DataAccess/Migrations/20250407063743_addIdentityTables.Designer.cs
@@ -4,6 +4,7 @@ using HarshBulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HarshBulky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250407063743_addIdentityTables")]
+    partial class addIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
